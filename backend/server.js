a04eeb8aeab2c4e2717837a04bca2ec9d24d18a1
chaudharyvicky564@gmail.com <chaudharyvicky564@gmail.com>
2024-11-24 08:48:00 +0530

    docker for backend

--- backend/server.js
+++ backend/server.js
@@ -11,7 +11,7 @@ const app = express()
 dotenv.config()
 // middlewares
 app.use(cors({
-    origin: 'http://localhost:5173', 
+    origin: 'http://localhost',
     credentials: true 
 }));
 app.use(morgan('dev'));
